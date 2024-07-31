57b1bddb16c8f41d413cdece08ff98d877f40016
Sander-dev <felipe.sander350@gmail.com>
2024-07-30 22:46:25 -0300

    fix: fix bugs

--- app/curriculo/page.js
+++ app/curriculo/page.js
@@ -6,7 +6,7 @@ export default function Curriculo() {
                 <p className="text-cyan-500">(38) 99738-8178 </p>
                 <p className="text-cyan-500">felipe.sander350@gmail.com</p>
             </div>
-            <div className="flex flex-col mt-5 w-[80vw] xl:w-[40vw]">
+            <div className="flex flex-col mt-5 w-[80vw] md:w-[70vw] lg:w-[60vw] xl:w-[50vw]">
                 <h1 className="text-xl mb-4">OBJETIVOS</h1>
                 <p className="text-white/70 max-sm:text-sm mb-4">Busco uma oportunidade para aplicar minha paixão e habilidades como desenvolvedor, enquanto
                     continuo a desenvolver meu conhecimento e experiência na área. Meu objetivo é não apenas
@@ -19,7 +19,10 @@ export default function Curriculo() {
                     <br />(IFNMG Campus Arinos)
                 </p>
                 <h1 className="text-xl mb-4">EXPERIÊNCIA</h1>
-                <h1 className="text-sm">01/2024 - Presente | Voluntariado de aprendizado em desenvolvimento de software</h1>
+                <h1 className="text-sm">05/2024 - 07/2024 | Desenvolvedor Front End - Marttech</h1>
+                <p className="text-white/70 mb-4">Minhas responsabilidades incluíam o desenvolvimento de aplicações web dinâmicas e responsivas utilizando tecnologias como ReactJS, TypeScript, styled-components, Material-UI e Azure DevOps. Trabalhei em colaboração com equipes multifuncionais para definir e entregar novas funcionalidades. Além disso, participei ativamente de metodologias ágeis, incluindo reuniões diárias e planejamento de sprints.
+                </p>
+                <h1 className="text-sm">08/2023 - 02/2024 | Voluntariado de aprendizado em desenvolvimento de software</h1>
                 <p className="text-white/70 mb-4">Participei de um projeto colaborativo que reuniu profissionais experientes do mercado com
                     iniciantes para desenvolver alguns projetos em equipe. Utilizei tecnologias como ReactJS, Node.js
                     e frameworks como Next.js e Express, trabalhando dentro de uma metodologia ágil com sprints e
@@ -29,10 +32,12 @@ export default function Curriculo() {
                     <div className="w-1/2">
                         <h1 className="text-xl mb-4">TECNOLOGIAS</h1>
                         <p className="text-white/70 mb-4">
+                            <li>TypeScript</li>
                             <li>JavaScript</li>
                             <li>ReactJS</li>
-                            <li>Node.js</li>
-                            <li>ExpressJS</li>
+                            <li>Tailwind CSS</li>
+                            <li>Styled-Component</li>
+                            <li>Git</li>
                             <li>NextJS</li>
                         </p>
                     </div>
@@ -42,8 +47,8 @@ export default function Curriculo() {
                             <li>JavaScript Puro – Danki Code</li>
                             <li>Node.js – Danki Code</li>
                             <li>HTML, CSS, JavaScript – Danki Code</li>
-                            <li>Python – Danki Code</li>
                             <li>Web Design – Danki Code</li>
+                            <li>ReactJS – NLW RocketSeat</li>
                         </p>
                     </div>
                 </div>
