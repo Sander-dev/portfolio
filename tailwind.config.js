fc8e0fcfe86392432b8a2eafb0efb8c19f2b72d0
Sander-dev <felipe.sander350@gmail.com>
2024-08-07 18:27:37 -0300

    feat: add config to flowbite in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    flowbite.content(),
   ],
   theme: {
     container: {
@@ -47,6 +48,9 @@ module.exports = {
       DEFAULT: '15px',
     },
   },
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [
+    require('tailwind-scrollbar'),
+    flowbite.plugin(),
+  ],
 };
 
