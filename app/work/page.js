885d542e3c83e4dbcdfd654b950e10be442a6f45
Sander-dev <felipe.sander350@gmail.com>
2024-08-07 18:28:36 -0300

    fix: fix the component WorkSlider e add in Work Page

--- app/work/page.js
+++ app/work/page.js
@@ -1,40 +1,10 @@
-'use client'
-
-import Link from "next/link";
-import Image from "next/image";
-import { motion } from "framer-motion";
-import { fadeIn } from '../../variants';
+import WorkSlider from "@/components/WorkSlider";
 
 export default function Work() {
-  const projects = [
-    { src: '/capizip.png', alt: 'Projeto 1', link: 'https://capi-zip.vercel.app/' },
-    { src: '/curarSaude.png', alt: 'Projeto 2', link: 'https://curarsaude.com.br/' },
-    { src: '/hero.png', alt: 'Projeto 3', link: 'https://hero-project-mu.vercel.app/' },
-  ];
-
   return (
-    <div className="flex flex-col w-full h-full items-center max-xl:pb-[80px]">
-      <motion.h1
-        variants={fadeIn('down', 0.2)}
-        initial='hidden'
-        animate='show'
-        className="h1 m-4">
-        Projetos
-      </motion.h1>
-      <div className="flex flex-wrap w-[98%] lg:w-5/6 bg-slate-900 rounded-t-md p-4 gap-4 justify-center overflow-y-auto h-full">
-        {projects.map((project, index) => (
-          <Link rel="noopener noreferrer" target="_blank" key={index} href={project.link}>
-            <div className="w-[480px] h-[300px] overflow-hidden rounded-md">
-              <Image
-                src={project.src}
-                width={480}
-                height={300}
-                alt={project.alt}
-                className='object-cover w-full h-full'
-              />
-            </div>
-          </Link>
-        ))}
+    <div className="w-full h-full flex justify-center items-center p-4">
+      <div className="w-full max-w-4xl h-auto sm:w-3/4 md:w-2/3 lg:w-1/2">
+        <WorkSlider />
       </div>
     </div>
   );
--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,51 +1,26 @@
-// data
-const workSlides = {
-  slides: [
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
-};
+import { Carousel } from "flowbite-react";
+import Link from "next/link";
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  const projects = [
+    { src: '/capizip.png', alt: 'Projeto 1', description: 'Encurtador de Link, onde quem encurtou o link pode ter acesso a informações sobre o link e quem o clicou (back-end desativada no momento)', link: 'https://capi-zip.vercel.app/' },
+    { src: '/curarSaude.png', alt: 'Projeto 2', description: 'Meu primeiro emprego, site para obter pontos e troca-los por prêmios, os pontos são obtidos com postagem ou por assistir vídeos de outros criadores', link: 'https://curarsaude.com.br/' },
+    { src: '/hero.png', alt: 'Projeto 3', description: 'Este projeto envolveu a criação de uma aplicação simples de listagem de heróis, sobre como consumir e integrar APIs externas no front-end usando React', link: 'https://hero-project-mu.vercel.app/' },
+  ];
+
+  return (
+    <div className="h-full w-full">
+      <Carousel pauseOnHover>
+        {projects.map((project, index) => (
+          <div key={index} className="relative">
+            <Link rel="noopener noreferrer" target="_blank" href={project.link}>
+              <img className="object-cover w-full h-[90%]" src={project.src} alt={project.alt} />
+            </Link>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default WorkSlider;
