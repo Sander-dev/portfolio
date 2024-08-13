4a7e94236b0e31e280a39a361303ff751fb9062b
Sander-dev <felipe.sander350@gmail.com>
2024-08-13 12:25:45 -0300

    fix: clean code

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,22 +1,34 @@
-'use client'
+'use client';
 
-import Link from "next/link";
-import { motion } from "framer-motion";
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
 
 export default function Contact() {
     return (
-        <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="flex flex-col justify-center items-center w-full h-screen p-4">
             <motion.h1
                 variants={fadeIn('down', 0.2)}
-                initial='hidden'
-                animate='show'
-                className="h2">
-                <span className="text-accent">Felipe Sander</span> <br />
-                <span>felipe.sander350@gmail.com</span> <br />
-                <Link rel="noopener noreferrer" target="_blank" className="text-accent" href={'https://github.com/Sander-dev/'}>github.com/Sander-dev</Link> <br />
-                <Link rel="noopener noreferrer" target="_blank" href={'https://www.linkedin.com/in/felipe-sander/'}>linkedin.com/in/felipe-sander</Link>
+                initial="hidden"
+                animate="show"
+                className="text-center text-2xl md:text-4xl font-bold space-y-2">
+                <span className="text-accent block">Felipe Sander</span>
+                <span className="block">felipe.sander350@gmail.com</span>
+                <Link
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-accent block hover:underline"
+                    href={'https://github.com/Sander-dev/'}>
+                    github.com/Sander-dev
+                </Link>
+                <Link
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="block hover:underline"
+                    href={'https://www.linkedin.com/in/felipe-sander/'}>
+                    linkedin.com/in/felipe-sander
+                </Link>
             </motion.h1>
         </div>
-    )
+    );
 }
